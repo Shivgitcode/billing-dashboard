660dfe1e19b572e0d822e4fa2e40122d76923d10
shweta wadhwa <shwetawadhwa75@gmail.com>
2024-08-05 20:37:32 +0530

    almost done

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,8 @@ function App() {
 
     return (
         <Routes>
-
             <Route path="/home" element={<Home></Home>}></Route>
             <Route path="/invoices" element={<Invoices></Invoices>}></Route>
-
             <Route path="/login" element={<Login></Login>}></Route>
             <Route path="/signup" element={<Signup></Signup>}></Route>
         </Routes>
